915f99c567d77b0cf0fbd12f557e7c98ac4fa92b
Roland4444 <turbopollux@rambler.ru>
2026-09-10 12:46:39 +0400

    fix versiob

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule McpGateway.MixProject do
     [
       app: :mcp_gateway,
       version: "0.1.0",
-      elixir: "~> 1.19",
+      elixir: ">= 1.15.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
